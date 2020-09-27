58fc60dd9597bbfc278d7bc8ec3afce6be613bc5
Leonardo Silva <leonardo.silva@mobimeo.com>
2020-09-27 21:42:44 +0200

    dependencies cleanup

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,8 +1,6 @@
 import sbt._
 
 object Versions {
-  val Analytics = "0.0.194"
-  val Circe = "0.13.0"
   val Logback = "1.2.3"
   val Logstash = "6.3"
   val ScalaLogging = "3.9.2"
@@ -12,11 +10,6 @@ object Versions {
 }
 
 object Dependencies {
-  val Avro = "io.confluent" % "kafka-avro-serializer" % "5.5.0"
-
-  val BetterFiles = "com.github.pathikrit" %% "better-files" % "3.9.1"
-
-  val CirceGeneric = "io.circe" %% "circe-generic" % Versions.Circe
 
   val Logging = Seq(
     "ch.qos.logback" % "logback-classic" % Versions.Logback,
@@ -27,8 +20,6 @@ object Dependencies {
   val ScalaTest = "org.scalatest" %% "scalatest" % Versions.ScalaTest
   val ScalaMock = "org.scalamock" %% "scalamock" % Versions.ScalaMock
 
-  val PrometheusMetrics = "io.micrometer" % "micrometer-registry-prometheus" % "1.5.4"
-
   val VertxCircuitBreaker = "io.vertx" % "vertx-circuit-breaker" % Versions.Vertx
   val VertxKafka = "io.vertx" % "vertx-kafka-client" % Versions.Vertx
   val VertxPrometheus = "io.vertx" % "vertx-micrometer-metrics" % Versions.Vertx
@@ -36,11 +27,6 @@ object Dependencies {
   val VertxWeb = "io.vertx" % "vertx-web" % Versions.Vertx
 
   val GtfsApi: Seq[ModuleID] = Logging ++ Seq(
-    AnalyticsDomain,
-    Avro,
-    BetterFiles,
-    CirceGeneric,
-    PrometheusMetrics,
     VertxCircuitBreaker,
     VertxKafka,
     VertxPrometheus,
