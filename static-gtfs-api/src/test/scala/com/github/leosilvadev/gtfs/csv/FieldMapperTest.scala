58fd089ea9f3835a9cd341f1091954d0ca506902
Leonardo Silva <leonardo.silva@mobimeo.com>
2020-09-28 23:03:26 +0200

    unit tests

---
+++ static-gtfs-api/src/test/scala/com/github/leosilvadev/gtfs/csv/FieldMapperTest.scala
@@ -0,0 +1,76 @@
+package com.github.leosilvadev.gtfs.csv
+
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.must.Matchers
+
+class FieldMapperTest extends AnyFunSpec with Matchers {
+
+  describe("Field mapper test") {
+    describe("Mapping the indexes") {
+      it("must map the field indexes even when not all mapped fields are present") {
+        val fieldsMapping = Map(
+          "f1" -> 0,
+          "f2" -> 1,
+          "f3" -> 2
+        )
+
+        val availableFields = List("f1", "f3")
+        val mapped = FieldMapper.mapIndexes(fieldsMapping, availableFields)
+        mapped.size mustBe 3
+        mapped(0) mustBe Some(0)
+        mapped(1) mustBe None
+        mapped(2) mustBe Some(1)
+      }
+
+      it("must map the field indexes even when all the present fields are not mapped") {
+        val fieldsMapping = Map(
+          "f1" -> 0,
+          "f3" -> 2
+        )
+
+        val availableFields = List("f1", "f4", "f3", "f2")
+        val mapped = FieldMapper.mapIndexes(fieldsMapping, availableFields)
+        mapped.size mustBe 2
+        mapped(0) mustBe Some(0)
+        mapped(2) mustBe Some(2)
+      }
+    }
+
+    describe("Mapping the columns") {
+      it("must map the columns even if some of them does not exist") {
+        Given
+        val columns = Array(
+          "000008012713",
+          "",
+          "Rangsdorf, Bahnhof",
+          "",
+          "52.294125000000",
+          "13.431112000000",
+          "0",
+          "900000245025",
+          "",
+          "",
+          ""
+        )
+
+        val mappedIndexes = Map(
+          0 -> Some(2),
+          1 -> Some(0),
+          2 -> Some(4),
+          3 -> Some(5),
+          4 -> None,
+          5 -> Some(7)
+        )
+        val mappedColumns = FieldMapper.mapColumns(columns, mappedIndexes)
+        mappedColumns.length mustBe 6
+        mappedColumns.head mustBe "Rangsdorf, Bahnhof"
+        mappedColumns(1) mustBe "000008012713"
+        mappedColumns(2) mustBe "52.294125000000"
+        mappedColumns(3) mustBe "13.431112000000"
+        mappedColumns(4) mustBe ""
+        mappedColumns(5) mustBe "900000245025"
+      }
+    }
+  }
+
+}
---
+++ static-gtfs-api/src/test/scala/com/github/leosilvadev/gtfs/csv/GtfsStopFileTest.scala
@@ -0,0 +1,78 @@
+package com.github.leosilvadev.gtfs.csv
+
+import com.github.leosilvadev.gtfs.GtfsStop
+import com.github.leosilvadev.gtfs.csv.FileReadOps._
+import com.github.leosilvadev.gtfs.csv.exceptions.MissingFieldsException
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.must.Matchers
+
+class GtfsStopFileTest extends AnyFunSpec with Matchers {
+
+  describe("Gtfs Agency file read") {
+    describe("Field validation") {
+      it("must read a file which contains all the required fields") {
+        GtfsStopFile.read("gtfs/complete_stops.txt") match {
+          case Left(ex) => fail("Failed because of unexpected error", ex)
+          case Right(stops) =>
+            stops must have size 8
+        }
+      }
+
+      it("must fail trying to read a file if any required field is missing") {
+        GtfsStopFile.read("gtfs/incomplete_stops.txt") match {
+          case Right(_) => fail("Parse should have failed by missing fields")
+          case Left(ex) =>
+            ex.asInstanceOf[MissingFieldsException].fields mustBe List("stop_id")
+        }
+      }
+
+      it("must fail trying to read a file if any required field is invalid") {
+        GtfsStopFile.read("gtfs/invalid_stops.txt") match {
+          case Left(ex) => fail("Failed because of unexpected error", ex)
+          case Right(stops) =>
+            stops must have size 8
+            stops.head.isRight mustBe true
+            stops(1).isRight mustBe true
+            stops(2).isRight mustBe true
+            stops(3).isRight mustBe true
+            stops(4).isRight mustBe true
+            stops(5) match {
+              case Right(_) => fail("Parse should have failed by invalid field")
+              case Left(ex) =>
+                ex.field mustBe "stop_id"
+            }
+            stops(6).isRight mustBe true
+            stops(7).isRight mustBe true
+        }
+      }
+    }
+
+    describe("Value assignment correctness") {
+      it("must assign all the values properly") {
+        GtfsStopFile.read("gtfs/complete_stops.txt") match {
+          case Left(ex) => fail("Failed because of unexpected error", ex)
+          case Right(stops) =>
+            stops.head mustBe Right(
+              GtfsStop(
+                id = "000008012713",
+                code = None,
+                name = Some("Rangsdorf, Bahnhof"),
+                description = None,
+                latitude = Some(52.294125000000),
+                longitude = Some(13.431112000000),
+                zoneId = None,
+                url = None,
+                locationType = Some(0),
+                parentStation = Some("900000245025"),
+                timezone = None,
+                wheelchairBoarding = None,
+                levelId = None,
+                platformCode = None
+              )
+            )
+        }
+      }
+    }
+  }
+
+}
